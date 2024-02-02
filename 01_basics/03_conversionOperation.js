942435397d96ff33c16cf8585a02966cee401aab
Priyanshu Gupta <121391773+priyanshu-gupta0503@users.noreply.github.com>
2024-02-02 10:25:46 +0000

    more one Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,39 @@ let someNuymber=44
 let stringNumber=String(someNuymber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+//------------------operations-------------------
+
+let value=3
+
+let negValue=-value
+// console.log(negValue);
+
+
+
+
+let str1= "heetesh"
+let str2= " Hitesh"
+let str3= str1 + str2
+console.log(str3);
+
+
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log("1" + 2+2);
+// console.log(1 + 2+"2");
+
+// console.log((3 + 4) * 5 % 3)
+
+console.log(+true);// answer is 1
+console.log(+""); // answer is 0
+
+
+let num1,num2 ,num3
+
+num1 = num2 = num3 =2+2
+
+let gameCounter =100
+ gameCounter++
+ console.log(gameCounter);
+ 
