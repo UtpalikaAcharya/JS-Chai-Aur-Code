f1cd21ca3a549303e2c4c7bdb6458916dffe973c
Priyanshu Gupta <121391773+priyanshu-gupta0503@users.noreply.github.com>
2024-02-02 08:49:33 +0000

    variables and  datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+// alert (3+3)  // we are using nodejs,not browser
+console.log(3
+    +3) // code readibilty is the main concern
+console.log("priyanshu")
+
+
+let name ="harry"
+let age=34;
+let isLoggedin="False";
+
+//number =>2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+//null => stand alone value //representation of empty value
+//undefined => value is not assigned
+// symbol => unique
+
+
+
+//object
+
+console.log( typeof null)// type of null is Object
+console.log( typeof undefined)// type of undefined is undefined
