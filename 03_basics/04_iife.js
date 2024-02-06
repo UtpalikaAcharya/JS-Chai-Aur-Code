f85c7c7f7407876cf98234c1fb32a3e360f992c3
Priyanshu Gupta <121391773+priyanshu-gupta0503@users.noreply.github.com>
2024-02-06 13:25:55 +0000

    IIfe

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
+
+
+// Do watch this again 05:48:15 from chai aur code video
